e8bfd9a38bde9dc263b8930cb0e5923a591c61ee
HK-RazieL <hkraziel@gmail.com>
2022-07-19 21:30:47 +0300

    Getting functions outside the component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,111 @@ import Table from "react-bootstrap/Table";
 
 const allowedExtensions = ["csv"];
 
+const checkHowManyDaysOverlap = (employee1, employee2) => {
+  const employee1StartDate = new Date(employee1.DateFrom);
+  let employee1EndDate;
+  const employee2StartDate = new Date(employee2.DateFrom);
+  let employee2EndDate;
+
+  if (employee1.DateTo === null || employee1.DateTo.toLowerCase() === "null") {
+    employee1EndDate = new Date();
+  } else {
+    employee1EndDate = new Date(employee1.DateTo)
+  }
+
+  if (employee2.DateTo === null || employee2.DateTo.toLowerCase() === "null") {
+    employee2EndDate = new Date();
+  } else {
+    employee2EndDate = new Date(employee2.DateTo)
+  }
+
+  if (employee1StartDate <= employee2EndDate && employee1EndDate >= employee2StartDate) {
+    const laterStart = employee1StartDate > employee2StartDate ? employee1StartDate : employee2StartDate;
+    const firstEnd = employee1EndDate < employee2EndDate ? employee1EndDate : employee2EndDate;
+    const timeTogether = Math.abs(firstEnd - laterStart);
+    return Math.floor(timeTogether / (1000 * 60 * 60 * 24));
+  } else {
+    return 0
+  }
+}
+
+const calculatingDays = (data, setFilteredData) => {
+  const employees = {};
+  const projects = {};
+
+  data.forEach(entry => {
+    if (!projects[entry.ProjectId]) {
+      projects[entry.ProjectId] = [entry];
+    } else {
+      projects[entry.ProjectId].push(entry);
+    }
+  });
+
+  for (let project of Object.values(projects)) {
+    for (let i = 0; i < project.length; i++) {
+      for (let j = i + 1; j < project.length; j++) {
+        let daysTogether = checkHowManyDaysOverlap(project[i], project[j])
+
+        if (daysTogether) {
+          if (!employees.hasOwnProperty(project[i].EmpID + "-" + project[j].EmpID)
+            && !employees.hasOwnProperty(project[j].EmpID + "-" + project[i].EmpID)) {
+            employees[project[i].EmpID + "-" + project[j].EmpID] = [{
+              project: project[0].ProjectId,
+              days: daysTogether
+            }];
+          } else if (employees[project[i].EmpID + "-" + project[j].EmpID]) {
+            if (employees[project[i].EmpID + "-" + project[j].EmpID].find((obj) => {
+              return obj.project === project[0].ProjectId;
+            })) {
+              employees[project[i].EmpID + "-" + project[j].EmpID].find((obj) => {
+                return obj.project === project[0].ProjectId;
+              }).days += daysTogether;
+            } else {
+              employees[project[i].EmpID + "-" + project[j].EmpID].push({
+                project: project[0].ProjectId,
+                days: daysTogether
+              });
+            }
+          } else if (employees[project[j].EmpID + "-" + project[i].EmpID]) {
+            if (employees[project[j].EmpID + "-" + project[i].EmpID].find((obj) => {
+              return obj.project === project[0].ProjectId;
+            })) {
+              employees[project[j].EmpID + "-" + project[i].EmpID].find((obj) => {
+                return obj.project === project[0].ProjectId;
+              }).days += daysTogether;
+            } else {
+              employees[project[j].EmpID + "-" + project[i].EmpID].push({
+                project: project[0].ProjectId,
+                days: daysTogether
+              });
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  setFilteredData(employees);
+}
+
+const findMostColaboration = (data, setCoupleWithMostDays) => {
+  let mostDays = {
+    couple: "",
+    days: 0,
+  };
+
+  for (let couple of Object.entries(data)){
+    let total = couple[1].reduce((total, days) => total + days.days, 0);
+
+    if (total > mostDays.days) {
+      mostDays.couple = couple[0];
+      mostDays.days = total
+    }
+  }
+
+  setCoupleWithMostDays(mostDays);
+}
+
 function App() {
   const [error, setError] = useState("");
   const [file, setFile] = useState("");
@@ -34,118 +139,15 @@ function App() {
     reader.onload = async ({ target }) => {
       const csv = Papa.parse(target.result, { header: true });
 
-      await calculatingDays(csv.data);
-      await findMostColaboration(filteredData);
+      calculatingDays(csv.data, setFilteredData);
     };
     
     reader.readAsText(file);
-
   };
 
-  const checkHowManyDaysOverlap = (employee1, employee2) => {
-    const employee1StartDate = new Date(employee1.DateFrom);
-    let employee1EndDate;
-    const employee2StartDate = new Date(employee2.DateFrom);
-    let employee2EndDate;
-
-    if (employee1.DateTo === null || employee1.DateTo.toLowerCase() === "null") {
-      employee1EndDate = new Date();
-    } else {
-      employee1EndDate = new Date(employee1.DateTo)
-    }
-
-    if (employee2.DateTo === null || employee2.DateTo.toLowerCase() === "null") {
-      employee2EndDate = new Date();
-    } else {
-      employee2EndDate = new Date(employee2.DateTo)
-    }
-
-    if (employee1StartDate <= employee2EndDate && employee1EndDate >= employee2StartDate) {
-      const laterStart = employee1StartDate > employee2StartDate ? employee1StartDate : employee2StartDate;
-      const firstEnd = employee1EndDate < employee2EndDate ? employee1EndDate : employee2EndDate;
-      const timeTogether = Math.abs(firstEnd - laterStart);
-      return Math.floor(timeTogether / (1000 * 60 * 60 * 24));
-    } else {
-      return 0
-    }
-  }
-
-  const calculatingDays = async (data) => {
-    const employees = {};
-    const projects = {};
-
-    data.forEach(entry => {
-      if (!projects[entry.ProjectId]) {
-        projects[entry.ProjectId] = [entry];
-      } else {
-        projects[entry.ProjectId].push(entry);
-      }
-    });
-
-    for await (let project of Object.values(projects)) {
-      for (let i = 0; i < project.length; i++) {
-        for (let j = i + 1; j < project.length; j++) {
-          let daysTogether = checkHowManyDaysOverlap(project[i], project[j])
-
-          if (daysTogether) {
-            if (!employees.hasOwnProperty(project[i].EmpID + "-" + project[j].EmpID)
-              && !employees.hasOwnProperty(project[j].EmpID + "-" + project[i].EmpID)) {
-              employees[project[i].EmpID + "-" + project[j].EmpID] = [{
-                project: project[0].ProjectId,
-                days: daysTogether
-              }];
-            } else if (employees[project[i].EmpID + "-" + project[j].EmpID]) {
-              if (employees[project[i].EmpID + "-" + project[j].EmpID].find((obj) => {
-                return obj.project === project[0].ProjectId;
-              })) {
-                employees[project[i].EmpID + "-" + project[j].EmpID].find((obj) => {
-                  return obj.project === project[0].ProjectId;
-                }).days += daysTogether;
-              } else {
-                employees[project[i].EmpID + "-" + project[j].EmpID].push({
-                  project: project[0].ProjectId,
-                  days: daysTogether
-                });
-              }
-            } else if (employees[project[j].EmpID + "-" + project[i].EmpID]) {
-              if (employees[project[j].EmpID + "-" + project[i].EmpID].find((obj) => {
-                return obj.project === project[0].ProjectId;
-              })) {
-                employees[project[j].EmpID + "-" + project[i].EmpID].find((obj) => {
-                  return obj.project === project[0].ProjectId;
-                }).days += daysTogether;
-              } else {
-                employees[project[j].EmpID + "-" + project[i].EmpID].push({
-                  project: project[0].ProjectId,
-                  days: daysTogether
-                });
-              }
-            }
-          }
-        }
-      }
-    }
-    
-    setFilteredData(employees);
-  }
-
-  const findMostColaboration = async (data) => {
-    let mostDays = {
-      couple: "",
-      days: 0,
-    };
-
-    for await (let couple of Object.entries(data)){
-      let total = couple[1].reduce((total, days) => total + days.days, 0);
-
-      if (total > mostDays.days) {
-        mostDays.couple = couple[0];
-        mostDays.days = total
-      }
-    }
-
-    setCoupleWithMostDays(mostDays);
-  }
+  useEffect(() => {
+    findMostColaboration(filteredData, setCoupleWithMostDays)
+  }, [filteredData])
 
   return (
     <div className="App">
